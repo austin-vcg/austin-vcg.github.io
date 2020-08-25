9dc6a9eb012093ecd3c91370413dc51f6bb2607d
Arthur J <jiarthur18@gmail.com>
2020-08-25 18:08:34 -0500

    editing For Members page to include member resources and open/close features

--- src/pages/ForMembers.jsx
+++ src/pages/ForMembers.jsx
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Navbar, Footer } from "../components";
 import { color_theme, text_theme } from "../theme";
 import useWindowDimensions from "../hooks/useWindowDimensions";
+import { useState } from "react";
 
 const HomePage = styled.div`
   background: transparent;
@@ -120,9 +121,60 @@ const GridItem = styled.div`
 
 const CalendarSection = styled.iframe`
   border: 0;
+  transition: 0.5s;
+`;
+
+const DropDownButton = styled.div`
+  display: flex;
+  width: 50vw;
+  height: 40px;
+  margin: 5px;
+  margin-bottom: 15px;
+  background-color: transparent;
+  align-content: center;
+  justify-content: center;
+  border-radius: 25px;
+  transition: 0.5s;
+
+  &:hover {
+    background-color: rgb(${color_theme.secondary});
+    & > * {
+      color: white;
+    }
+  }
+`;
+
+const ButtonIcon = styled(FontAwesomeIcon)`
+  font-size: 40px;
+  color: black;
+  margin-bottom: 20px;
+  color: rgb(${color_theme.secondary});
+  @media screen and (max-width: 600px), (max-height: 600px) {
+    margin-bottom: 10px;
+    font-size: 35px;
+  }
+  transition: 0.5s;
+`;
+
+const MemberResourcesFrame = styled.iframe`
+  width: 60vw;
+  height: ${(props) => (props.expanded ? `70vh` : `0vh`)};
+  border: ${(props) => (props.expanded ? `2px ${color_theme.primary}` : `0`)};
+  transition: 0.5s;
 `;
 
 const ForMembers = () => {
+  const [viewFiles, setViewFiles] = useState(false);
+  const [viewCalendar, setViewCalendar] = useState(true);
+
+  const openFiles = () => {
+    setViewFiles(!viewFiles);
+  };
+
+  const openCalendar = () => {
+    setViewCalendar(!viewCalendar);
+  };
+
   const { width, height } = useWindowDimensions();
   return (
     <HomePage>
@@ -134,11 +186,21 @@ const ForMembers = () => {
         <Section>
           <SectionIcon icon={["far", "calendar"]} />
           <SectionH1>Organization Calendar</SectionH1>
+          <SectionText>
+            Here's a list of upcoming events for Texas VCG Members.
+          </SectionText>
+          <DropDownButton onClick={() => openCalendar()}>
+            {viewCalendar ? (
+              <ButtonIcon icon={["fas", "chevron-up"]} />
+            ) : (
+              <ButtonIcon icon={["fas", "chevron-down"]} />
+            )}
+          </DropDownButton>
           <CalendarSection
             src="https://calendar.google.com/calendar/embed?src=21j1cj1b2h7ank71t9lsc6msps%40group.calendar.google.com&ctz=America%2FLos_Angeles"
             title="Upcoming Events"
             width={width > 500 ? width * 0.75 : 433}
-            height={height * 0.75}
+            height={viewCalendar ? height * 0.75 : 0}
             frameborder="0"
             scrolling="no"
           />
@@ -149,7 +211,25 @@ const ForMembers = () => {
         <Section>
           <SectionIcon icon={["far", "lightbulb"]} />
           <SectionH1>Member Resources</SectionH1>
-          <SectionText>Check later for new resources!</SectionText>
+          <SectionText>
+            Click below for documents that are available to everyone to build up
+            their own skills in venture capital. Feel free to contact us if
+            there is anything you'd like to add.
+          </SectionText>
+          <DropDownButton onClick={() => openFiles()}>
+            {viewFiles ? (
+              <ButtonIcon icon={["fas", "chevron-up"]} />
+            ) : (
+              <ButtonIcon icon={["fas", "chevron-down"]} />
+            )}
+          </DropDownButton>
+
+          <MemberResourcesFrame
+            expanded={viewFiles}
+            title="Member Resources"
+            src="https://drive.google.com/embeddedfolderview?id=1hlI65ivWTPlee_UYBYHWXRRIg9b87dTT#list"
+          ></MemberResourcesFrame>
+          {/* <SectionText>Check later for new resources!</SectionText> */}
         </Section>
       </Body>
 
